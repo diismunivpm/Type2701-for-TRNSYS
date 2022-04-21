3f612bcb6857502734ea85a8410525d64d25aea5
diism.univpm <103246194+diismunivpm@users.noreply.github.com>
2022-04-21 12:09:25 +0200

    Delete DLL compiled/Compilers/Debug directory

--- DLL compiled/Compilers/Debug/type2701__genmod.f90
+++
@@ -1,9 +0,0 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Wed Apr 13 10:47:06 2022
-        ! This source file is for reference only and may not completely
-        ! represent the generated interface used by the compiler.
-        MODULE TYPE2701__genmod
-          INTERFACE 
-            SUBROUTINE TYPE2701
-            END SUBROUTINE TYPE2701
-          END INTERFACE 
-        END MODULE TYPE2701__genmod
